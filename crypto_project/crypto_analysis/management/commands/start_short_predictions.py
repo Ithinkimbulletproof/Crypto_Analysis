0942481018bed4c0d9d6ad599e212cab8b354a28
Alexandr Muhametchin <devilmaker228@gmail.com>
2025-01-25 12:28:52 +0500

    Checkpoint 2.4.1

--- crypto_project/crypto_analysis/management/commands/start_short_predictions.py
+++ crypto_project/crypto_analysis/management/commands/start_short_predictions.py
@@ -1,7 +1,8 @@
+import logging
 from django.core.management.base import BaseCommand
 from crypto_analysis.services.data_fetcher import fetch_data
 from crypto_analysis.services.data_preprocessing import process_all_indicators
-import logging
+from crypto_analysis.services.short_predictions import run_short_predictions
 
 logger = logging.getLogger(__name__)
 
@@ -20,8 +21,10 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS("Запуск обработки индикаторов..."))
             process_all_indicators()
             self.stdout.write(
-                self.style.SUCCESS("Все индикаторы успешно рассчитаны и сохранены")
-            )
+                self.style.SUCCESS("Все индикаторы успешно рассчитаны и сохранены"))
+            self.stdout.write(self.style.SUCCESS("Запуск расчёта предсказаний..."))
+            run_short_predictions()
+            self.style.SUCCESS("Все предсказания успешно рассчитаны и сохранены")
         except Exception as e:
             logger.error(f"Ошибка при выполнении команды: {str(e)}")
             self.stdout.write(self.style.ERROR(f"Ошибка: {str(e)}"))
